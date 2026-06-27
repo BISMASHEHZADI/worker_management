c5e87466ce9c1dc2f6a6c83d128cd26299fe2c6f
BISMASHEHZADI <157670590+BISMASHEHZADI@users.noreply.github.com>
2026-06-27 21:50:02 +0500

    Add login and logout paths to URLs

--- worker_managment/main_app/urls.py
+++ worker_managment/main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.custom_login, name='login'),
+    path('logout/', views.custom_logout, name='logout'),
     path('', views.home, name='home'),
     path('workers/', views.workers, name='workers'),
     path('tasks/', views.tasks, name='tasks'),
